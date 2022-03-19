6c194c4b7e6fc2020a6ad91e46e3288a55bcc492
Fengyu He <banxiabian@gmail.com>
2022-03-19 22:38:34 +0800

    发送私信完成

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -188,4 +188,8 @@ public class UserService implements CommunityConstant {
         return userMapper.updatePassword(userId, password);
     }
 
+    public User findUserByName(String username) {
+        return userMapper.selectByName(username);
+    }
+
 }
