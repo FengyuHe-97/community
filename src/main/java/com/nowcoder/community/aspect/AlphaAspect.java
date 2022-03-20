25d941a6fea91416d168a78a9da13e540778d0fd
Fengyu He <banxiabian@gmail.com>
2022-03-20 18:54:41 +0800

    小试AOP 实现统一记录日志

---
+++ src/main/java/com/nowcoder/community/aspect/AlphaAspect.java
@@ -0,0 +1,51 @@
+package com.nowcoder.community.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
+//@Component
+//@Aspect
+public class AlphaAspect {
+
+    //AOP演示
+    //定义切点
+    //第一个*代表 返回值；service 下所有的类 所有方法 的所有参数
+    @Pointcut("execution(* com.nowcoder.community.service.*.*(..))")
+    public void pointcut() {
+
+    }
+
+    //在连接点开始做xxx
+    @Before("pointcut()")
+    public void before() {
+        System.out.println("before");
+    }
+
+    //在连接点结束做xxx
+    @After("pointcut()")
+    public void after() {
+        System.out.println("after");
+    }
+
+    //返回值后做xxx
+    @AfterReturning("pointcut()")
+    public void afterRetuning() {
+        System.out.println("afterRetuning");
+    }
+
+    //抛出异常时做xxx
+    @AfterThrowing("pointcut()")
+    public void afterThrowing() {
+        System.out.println("afterThrowing");
+    }
+
+    //环绕： 前后 都可以
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("around before");
+        Object obj = joinPoint.proceed();
+        System.out.println("around after");
+        return obj;
+    }
+
+}
